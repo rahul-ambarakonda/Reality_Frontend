296350571429d3b5dcc8f7c171c43a067d31afc9
Rahul Ambarakonda <35566693+rahul-ambarakonda@users.noreply.github.com>
2026-07-30 14:40:17 +0530

    feat: integrate Layout component into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
-import { Routes, Route, Link as RouterLink } from 'react-router-dom';
-import { AppBar, Toolbar, Button, Typography, Container } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Reality Frontend
-          </Typography>
-          <Button color="inherit" component={RouterLink} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/about">
-            About
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="md" sx={{ mt: 4 }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Container>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
