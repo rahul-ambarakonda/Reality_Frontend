ebe31be3d51e00d24bfda32e7d7d15afcd9761bc
Rahul Ambarakonda <35566693+rahul-ambarakonda@users.noreply.github.com>
2026-07-30 14:40:49 +0530

    feat: Add /projects/:id route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import ProjectDetailsPage from './pages/ProjectDetailsPage'; // Import the new page
 import Layout from './components/Layout';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects/:id" element={<ProjectDetailsPage />} /> {/* Add the new route */}
       </Routes>
     </Layout>
   );
